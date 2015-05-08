e5b83a622f023cb400538c754208fbdb917d6d90
Sara Simon <smbsimon@gmail.com>
2015-05-07 21:00:36 -0400

    Rename functions for clarity

--- leap/year_two.py
+++ leap/year_two.py
@@ -1,11 +1,11 @@
 def is_leap_year(year):
-    return check_by_four_hundred(year) or check_by_one_hundred(year) and check_by_four(year)
+    return divisible_by_four_hundred(year) or not_divisible_by_one_hundred(year) and divisible_by_four(year)
 
-def check_by_four_hundred(year):
+def divisible_by_four_hundred(year):
     return year % 400 == 0
 
-def check_by_one_hundred(year):
+def not_divisible_by_one_hundred(year):
     return year % 100 != 0
 
-def check_by_four(year):
+def divisible_by_four(year):
     return year % 4 == 0
