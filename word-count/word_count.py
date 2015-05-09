ef68da72fec1b43607a53c66d1a49e9e19e28cb4
Sara Simon <smbsimon@gmail.com>
2015-05-09 19:25:18 -0400

    Refactor to better handle very large phrases.

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,8 +1,11 @@
 def word_count(phrase):
-    dictionary = {}
+    collection = {}
     split_phrase = phrase.split()
 
     for i in split_phrase:
-        dictionary[i] = split_phrase.count(i)
+        if i not in collection:
+            collection[i] = 1
+        else:
+            collection[i] += 1
 
-    return dictionary
+    return collection
