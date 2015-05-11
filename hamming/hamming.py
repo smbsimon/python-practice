2ca618c1ad642cf654ab7f0edac47f5a254ccca3
Sara Simon <smbsimon@gmail.com>
2015-05-11 17:45:52 -0400

    Rename for clarity

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,7 +1,7 @@
 def distance(strand1, strand2):
     hamming = 0
 
-    for a, b in zip(strand1, strand2):
-        if a != b:
+    for x, y in zip(strand1, strand2):
+        if x != y:
             hamming +=1
     return hamming
