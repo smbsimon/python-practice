adb476c6e0543d07d9d2b307f2174cd018178c38
Sara Simon <smbsimon@gmail.com>
2015-05-11 17:36:57 -0400

    Refactor to use zip

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,7 +1,7 @@
 def distance(a, b):
     hamming = 0
 
-    for i in range(len(a)):
-        if a[i] != b[i]:
-            hamming += 1
+    for a, b in zip(a, b):
+        if a != b:
+            hamming +=1
     return hamming
