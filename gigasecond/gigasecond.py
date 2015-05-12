e3bce13389e4fb66de8679acd1b64a61e4a9e292
Sara Simon <smbsimon@gmail.com>
2015-05-11 21:20:33 -0400

    Refactor to import only what I need, then tidy up gigasecond calculation to avoid passing variables.

--- gigasecond/gigasecond.py
+++ gigasecond/gigasecond.py
@@ -1,7 +1,4 @@
-import datetime
+from datetime import timedelta
 
 def add_gigasecond(start_date):
-    giga_difference = datetime.timedelta(seconds=1e9)
-    end_date = start_date + giga_difference
-    
-    return end_date
+    return start_date + timedelta(seconds=1e9)
