3b845ee5a54e2c36a00ac03897accfe7ee8d9f84
Sara Simon <smbsimon@gmail.com>
2015-05-24 15:06:50 -0400

    Clean up weekday index

--- meetup/meetup.py
+++ meetup/meetup.py
@@ -2,13 +2,11 @@ import datetime
 import calendar
 import re
 
-weekday_to_number = {weekday: number
-    for (number, weekday) in enumerate(calendar.day_name)}
-
 def meetup_day(year, month, weekday, attribute):
+    days_of_the_week = [d for d in calendar.day_name]
+    weekday_index = days_of_the_week.index(weekday)
 
     month_split_by_weeks = calendar.monthcalendar(year, month)
-    weekday_number = weekday_to_number[weekday]
     dates_split_by_weekdays = []
 
     for i in month_split_by_weeks:
@@ -16,13 +14,13 @@ def meetup_day(year, month, weekday, attribute):
 
     if any(char.isdigit() for char in attribute):
         attribute = int(re.sub(r'\D', "", attribute))
-        if dates_split_by_weekdays[weekday_number][0] > 0:
+        if dates_split_by_weekdays[weekday_index][0] > 0:
             attribute = attribute - 1
-        date = dates_split_by_weekdays[weekday_number][attribute]
+        date = dates_split_by_weekdays[weekday_index][attribute]
     elif attribute == 'last':
-        date = dates_split_by_weekdays[weekday_number][-1]
+        date = dates_split_by_weekdays[weekday_index][-1]
     elif attribute == 'teenth':
-        for i in dates_split_by_weekdays[weekday_number]:
+        for i in dates_split_by_weekdays[weekday_index]:
             if i >= 13:
                 date = i
                 break
